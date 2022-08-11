cbbce00adc2ff336dc4e836837b028f45a221ff9
nukosuke <nukosuke@users.noreply.github.com>
2022-08-11 16:16:59 +0900

    Fix minimum Elixir version and add dev deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Kanji.MixProject do
     [
       app: :kanji,
       version: "0.0.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,7 +21,7 @@ defmodule Kanji.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
